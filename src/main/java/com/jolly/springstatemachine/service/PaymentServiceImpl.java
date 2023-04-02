42922f8d8f244bd0ac3ec0c4ec17fac99753605f
jolly6 <98voonfusing@gmail.com>
2023-04-02 15:15:16 +0800

    Added restore state machine function in PaymentServiceImpl

--- src/main/java/com/jolly/springstatemachine/service/PaymentServiceImpl.java
+++ src/main/java/com/jolly/springstatemachine/service/PaymentServiceImpl.java
@@ -7,6 +7,7 @@ import com.jolly.springstatemachine.repository.PaymentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.statemachine.StateMachine;
 import org.springframework.statemachine.config.StateMachineFactory;
+import org.springframework.statemachine.support.DefaultStateMachineContext;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
@@ -24,16 +25,42 @@ public class PaymentServiceImpl implements PaymentService{
 
     @Override
     public StateMachine<PaymentState, PaymentEvent> preAuth(Long paymentId) {
+        StateMachine<PaymentState, PaymentEvent> sm = build(paymentId);
         return null;
     }
 
     @Override
     public StateMachine<PaymentState, PaymentEvent> authorizePayment(Long paymentId) {
+        StateMachine<PaymentState, PaymentEvent> sm = build(paymentId);
         return null;
     }
 
     @Override
     public StateMachine<PaymentState, PaymentEvent> declineAuth(Long paymentId) {
+        StateMachine<PaymentState, PaymentEvent> sm = build(paymentId);
         return null;
+    }
+
+    // to rehydrate state machine
+    private StateMachine<PaymentState, PaymentEvent> build(Long paymentId) {
+        Payment payment = paymentRepository.findById(paymentId).orElse(null);
+
+        if (payment == null) return null;
+
+        StateMachine<PaymentState, PaymentEvent> sm = stateMachineFactory.getStateMachine(Long.toString(payment.getId()));
+
+        sm.stop();
+
+        // reset state to the state set in DB
+        sm.getStateMachineAccessor()
+                .doWithAllRegions(sma -> {
+                    sma.resetStateMachine(new DefaultStateMachineContext<>(payment.getState(), null, null
+                    , null
+                    ));
+                });
+
+        sm.start();
+
+        return sm;
     }
 }
