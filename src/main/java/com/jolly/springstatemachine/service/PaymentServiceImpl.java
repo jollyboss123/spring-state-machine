27c969dbb80485238a3b637a1daefc448a5ae509
jolly6 <98voonfusing@gmail.com>
2023-04-02 15:24:16 +0800

    Setup message event to be sent to the state machine in PaymentServiceImpl

--- src/main/java/com/jolly/springstatemachine/service/PaymentServiceImpl.java
+++ src/main/java/com/jolly/springstatemachine/service/PaymentServiceImpl.java
@@ -5,6 +5,8 @@ import com.jolly.springstatemachine.domain.PaymentEvent;
 import com.jolly.springstatemachine.domain.PaymentState;
 import com.jolly.springstatemachine.repository.PaymentRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.statemachine.StateMachine;
 import org.springframework.statemachine.config.StateMachineFactory;
 import org.springframework.statemachine.support.DefaultStateMachineContext;
@@ -13,6 +15,7 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 @Service
 public class PaymentServiceImpl implements PaymentService{
+    private static final String PAYMENT_HEADER_ID = "payment_id";
 
     private final PaymentRepository paymentRepository;
     private final StateMachineFactory<PaymentState, PaymentEvent> stateMachineFactory;
@@ -26,25 +29,50 @@ public class PaymentServiceImpl implements PaymentService{
     @Override
     public StateMachine<PaymentState, PaymentEvent> preAuth(Long paymentId) {
         StateMachine<PaymentState, PaymentEvent> sm = build(paymentId);
+
+        //TODO: throw custom error state machine not found
+        if (sm == null) return null;
+
+        sendEvent(paymentId, sm, PaymentEvent.PRE_AUTHORIZE);
+
         return null;
     }
 
     @Override
     public StateMachine<PaymentState, PaymentEvent> authorizePayment(Long paymentId) {
         StateMachine<PaymentState, PaymentEvent> sm = build(paymentId);
+
+        if (sm == null) return null;
+
+        sendEvent(paymentId, sm, PaymentEvent.AUTH_APPROVED);
+
         return null;
     }
 
     @Override
     public StateMachine<PaymentState, PaymentEvent> declineAuth(Long paymentId) {
         StateMachine<PaymentState, PaymentEvent> sm = build(paymentId);
+
+        if (sm == null) return null;
+
+        sendEvent(paymentId, sm, PaymentEvent.AUTH_DECLINED);
+
         return null;
+    }
+
+    private void sendEvent(Long paymentId, StateMachine<PaymentState, PaymentEvent> sm, PaymentEvent event) {
+        Message<PaymentEvent> msg = MessageBuilder.withPayload(event)
+                .setHeader(PAYMENT_HEADER_ID, paymentId)
+                .build();
+
+        sm.sendEvent(msg);
     }
 
     // to rehydrate state machine
     private StateMachine<PaymentState, PaymentEvent> build(Long paymentId) {
         Payment payment = paymentRepository.findById(paymentId).orElse(null);
 
+        //TODO: throw custom error payment not found
         if (payment == null) return null;
 
         StateMachine<PaymentState, PaymentEvent> sm = stateMachineFactory.getStateMachine(Long.toString(payment.getId()));
