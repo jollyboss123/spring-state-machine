79d3854facd710bb0f596a57aa606f0e81527e9f
jolly6 <98voonfusing@gmail.com>
2023-04-02 15:02:21 +0800

    Added PaymentService and PaymentServiceImpl

---
+++ src/main/java/com/jolly/springstatemachine/service/PaymentService.java
@@ -0,0 +1,16 @@
+package com.jolly.springstatemachine.service;
+
+import com.jolly.springstatemachine.domain.Payment;
+import com.jolly.springstatemachine.domain.PaymentEvent;
+import com.jolly.springstatemachine.domain.PaymentState;
+import org.springframework.statemachine.StateMachine;
+
+public interface PaymentService {
+    Payment newPayment(Payment payment);
+
+    StateMachine<PaymentState, PaymentEvent> preAuth(Long paymentId);
+
+    StateMachine<PaymentState, PaymentEvent> authorizePayment(Long paymentId);
+
+    StateMachine<PaymentState, PaymentEvent> declineAuth(Long paymentId);
+}
